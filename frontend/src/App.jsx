75feb3c88ce04f38a3df04ca2e2979dff578e7aa
Anurag <it.anuragadhikari>
2024-07-03 00:58:19 +0530

    home page, cart page, place order page, login/signup popup ALL DONE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import {  BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
+import LoginPopup from './components/LoginPopup/LoginPopup'
 
 const App = () => {
+  // to show popup of login 
+  const [showLogin, setShowLogin] = useState(false)
+
   return (
     <>
+    {/* if false it will popup else not */}
+    {
+      showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>
+    }
       <div className='app'>
-        <Navbar/>
+        <Navbar setShowLogin={setShowLogin}/>
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/cart' element={<Cart/>} />
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -8,26 +8,38 @@ const StoreContextProvider = (props) => {
     // for cart item to store its state
     const [cartItems, setCartItems] = useState({})
 
-    const addToCart = async(itemId) => {
+    const addToCart = async (itemId) => {
 
         // if item is not in cart, add it with quantity 1
-        if (!cartItems[itemId]){
-            setCartItems((prev) => ({...prev, [itemId]:1}))
-        } 
+        if (!cartItems[itemId]) {
+            setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
+        }
         // if item is in cart, increase its quantity by 1
-        else{
-            setCartItems((prev) => ({...prev, [itemId]:prev[itemId]+1}))
+        else {
+            setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
         }
     }
 
     // for removing item from cart
     const removeFromCart = async (itemId) => {
-        setCartItems((prev) => ({...prev, [itemId]:prev[itemId]-1}))
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
     }
 
-    useEffect(() => {
-        console.log(cartItems);
-    },[cartItems])
+    // useEffect(() => {
+    //     console.log(cartItems);
+    // },[cartItems])
+
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+
+        for (let item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo = food_list.find((product) => product._id === item);
+                totalAmount += itemInfo.price * cartItems[item]
+            }
+        }
+        return totalAmount;
+    }
 
     const contextValue = {
         food_list,
@@ -35,8 +47,9 @@ const StoreContextProvider = (props) => {
         setCartItems,
         addToCart,
         removeFromCart,
+        getTotalCartAmount
     }
-    
+
     return (
         <StoreContext.Provider value={contextValue}>
             {props.children}
